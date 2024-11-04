e53539503a548a4d144fbabadf45f76615c48384
swgc-shota <137166117+swgc-shota@users.noreply.github.com>
2024-11-04 22:42:28 +0900

    Add a condition to skip popper display when text and URL are unchanged

--- src/content/UrlPopper/getPopperTexts.ts
+++ src/content/UrlPopper/getPopperTexts.ts
@@ -1,8 +1,9 @@
-import { State } from "vanjs-core";
+import { State } from 'vanjs-core';
 
 const updatePopperTexts = (
   popperTexts: State<PopperTexts>,
-  source: PopSource
+  source: PopSource,
+  timerId: NodeJS.Timeout
 ): void => {
   let newTexts = null as null | PopperTexts;
   if (source instanceof Document) {
@@ -12,22 +13,31 @@ const updatePopperTexts = (
   } else if (source instanceof HTMLImageElement) {
     newTexts = extractSrcAndAlt(source);
   } else {
-    throw new Error("Invalid source type");
+    throw new Error('Invalid source type');
   }
+
+  if (
+    newTexts.text === popperTexts.val.text &&
+    newTexts.url === popperTexts.val.url
+  ) {
+    clearTimeout(timerId);
+    return;
+  }
+
   popperTexts.val = { ...(newTexts as PopperTexts) };
 };
 const isEmpty = (target: string) => /^\s*$/.test(target);
 const getPageUrlAndTitle = (doc: Document): PopperTexts => {
   const titleElement =
-    doc.querySelector("title") ||
-    doc.querySelector("h1") ||
-    doc.querySelector("h2");
+    doc.querySelector('title') ||
+    doc.querySelector('h1') ||
+    doc.querySelector('h2');
 
   const isTitleExist =
     titleElement &&
     titleElement.textContent &&
     !isEmpty(titleElement.textContent);
-  const text = isTitleExist ? titleElement.textContent : "No title";
+  const text = isTitleExist ? titleElement.textContent : 'No title';
 
   return {
     text: text as string,
@@ -37,10 +47,10 @@ const getPageUrlAndTitle = (doc: Document): PopperTexts => {
 
 const extractHrefAndText = (dom: HTMLAnchorElement): PopperTexts => {
   const isTextExist = dom.textContent && !isEmpty(dom.textContent);
-  const text = (isTextExist ? dom.textContent : "No text content") as string;
+  const text = (isTextExist ? dom.textContent : 'No text content') as string;
 
   const isUrlExist = dom.href && !isEmpty(dom.href);
-  const url = isUrlExist ? dom.href : "No href attribute";
+  const url = isUrlExist ? dom.href : 'No href attribute';
   return {
     text: text as string,
     url,
@@ -49,10 +59,10 @@ const extractHrefAndText = (dom: HTMLAnchorElement): PopperTexts => {
 
 const extractSrcAndAlt = (dom: HTMLImageElement): PopperTexts => {
   const isAltExist = dom.alt && !isEmpty(dom.alt);
-  const text = isAltExist ? dom.alt : "No alt attribute";
+  const text = isAltExist ? dom.alt : 'No alt attribute';
 
   const isUrlExist = dom.src && !isEmpty(dom.src);
-  const url = isUrlExist ? dom.src : "No src attribute";
+  const url = isUrlExist ? dom.src : 'No src attribute';
 
   return {
     text,
