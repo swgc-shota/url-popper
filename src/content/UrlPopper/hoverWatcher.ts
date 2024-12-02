f05bfa3607b63801477e6d36c72dc9e29192d78c
swgc-shota <137166117+swgc-shota@users.noreply.github.com>
2024-12-02 21:00:07 +0900

    Fix a bug where mouse over events on heading tags within anchor tags are ignored

--- src/content/UrlPopper/hoverWatcher.ts
+++ src/content/UrlPopper/hoverWatcher.ts
@@ -3,34 +3,52 @@ import { State } from 'vanjs-core';
 import { selectComponent } from '../utils/utils';
 
 const createHoverWatcher = (popperTexts: State<PopperTexts>) => {
-  const listener = (e: MouseEvent) => {
-    if (e.target == null) {
-      return;
-    }
+  let prevPopTarget: PopSource | null = null;
 
-    const currentHoverElement = e.target as Element;
-    const target = currentHoverElement.closest(
-      'a, h1, h2, h3, h4, h5 ,h6,img'
+  const handleMouseover = (event: MouseEvent) => {
+    const target = event.target as Element | null;
+    if (!target) return;
+
+    const popTarget = findPopTarget(target);
+    if (!popTarget || popTarget === prevPopTarget) return;
+
+    updatePopper(popTarget);
+    prevPopTarget = popTarget;
+  };
+  const findPopTarget = (element: Element): PopSource | null => {
+    const anchor = element.closest('a');
+    if (anchor) return anchor;
+
+    const imgOrHeading = element.closest(
+      'h1, h2, h3, h4, h5, h6, img'
     ) as PopSource | null;
+    if (!imgOrHeading) return null;
+
     if (
-      target === null ||
-      (target instanceof HTMLHeadingElement &&
-        target.id === '' &&
-        target.parentElement!.id === '')
+      imgOrHeading instanceof HTMLHeadingElement &&
+      !isValidHeading(imgOrHeading)
     ) {
-      return;
+      return null;
     }
 
+    return imgOrHeading;
+  };
+
+  const isValidHeading = (element: HTMLHeadingElement) => {
+    return element.id !== '' || element.parentElement!.id !== '';
+  };
+
+  const updatePopper = (target: PopSource) => {
     selectComponent('url-popper').classList.remove('mono');
     showPopper(popperTexts, target);
   };
 
   const on = () => {
-    document.addEventListener('mouseover', listener);
+    document.addEventListener('mouseover', handleMouseover);
   };
 
   const off = () => {
-    document.removeEventListener('mouseover', listener);
+    document.removeEventListener('mouseover', handleMouseover);
   };
   return {
     on,
